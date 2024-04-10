8d2ccd64ee4f047944ef30be4453644073afbd17
B Water <brennw47@gmail.com>
2024-04-10 12:10:32 -0600

    Update logic.js/index.html
    Updated the logic.js file to have an event listener for the search button that grabs the id, prevents default form behavior for that id when submitted/clicked, takes the city input for the api function line 34, trims the value to display it properly, if there is no data in the search bar when clicked then alert to do so 23-30, then the function to get the weather data is set up to use the apikey and the url parameters will take in the city and use the apikey, then the data is fetched and returned as an object in the console. Commented in the other functions so that I can put them into the next function for the second fetch using the 5 day forecast api then locally stored the city name, and set the current day data to the date of the the data. Then started making the function to display the weather data for the 5day forecast however I dont have an object stored locally to work with yet so I can set the data to the paragraph tags for the 5 day forecast. Made a function to save cities and push the cities if there is one to the local storage. then made a function to display the saved cities by parseing the data from the local storage by the array. Updated the index.html so that there is ids on the cards for the 5 day data so I can target them in the javascript to populate as well as the section for the current weather now there is an id there to for the daily forecast.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,41 +1,81 @@
-// dd699e84055222330cd4dae0d505a590 This is the API key for OpenWeatherMap
 // q: The query parameter, where we'll add the city variable. appid: The application id or key, where we'll add the API key variable.
 //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} API call for openweathermap by city name
 //https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key} API call for openweathermap by city name and country code
 // API key for OpenWeatherMap although API is randomly generated
-const APIKey = "dd699e84055222330cd4dae0d505a590";
-
-
-const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-fetch(queryURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    });
-
-// city Search
-// search History
-let search;
-let city;
-let country;
-let date;
-let temp;
-let wind;
-let humidity;
-
-// Where we call the const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`; to get the data from the API
 // Yes when variables are assigned we will click the search button and the event listener will be triggered  and the function will call the variables to be displayed spliced concatted.
-// fetch Response then 'then' data 'then' event
-// Here we are building the URL we need to query the database
-// {
-//     "id": 5771826,
-//     "name": "Bountiful",
-//     "state": "UT",
-//     "country": "US",
-//     "coord": {
-//         "lon": -111.880768,
-//         "lat": 40.889389
-//     }
-// },
+// let search;
+// let city;
+// let country;
+// let date;
+// let temp;
+// let wind;
+// let humidity;
+
+// Event listener for search button
+document
+  .getElementById("searchCity")
+  .addEventListener("submit", function (event) {
+    // Prevent the default form submission
+    event.preventDefault();
+    const cityInput = document.getElementById("cityInput");
+    const city = cityInput.value.trim();
+    // If the user enters a city name, fetch the weather forecast for that city
+    if (city !== "") {
+      getWeatherData(city);
+      // Clear the input field after the user submits the form
+      cityInput.value = "";
+    } else {
+      // If the user doesn't put anything in the input field, alert them to enter a city name.
+      alert("Please enter a city name");
+    }
+  });
+
+// API key for OpenWeatherMap and fetch the url with the city variable and API key variable
+function getWeatherData(city) {
+  // API Key which allows us to make request to the server.
+  const APIKey = "dd699e84055222330cd4dae0d505a590";
+  const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`; // Geo coordinates for the city (lat and lon)
+
+  // Fetch the data from the API
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    // Display the data in console
+    .then(function (data) {
+      console.log(data);
+      // make variables for the lat and lon from the data and run a second fetch to get the 5 day forecast using the lat and lon
+      // Move the function below into the second fetch
+      // Display the data on the page
+      displayWeatherData(data); // Should handle the 5 days
+      // Save the city to local storage so it persists on page reload
+      saveCity(city); // Save the searched city to the past searches local storage array
+      // Render after page reload
+      displaySavedCities(); // Displays the past searches
+      // Display the array of data on the page
+      displayCurrentDayData(data.list[0], city); // This is the current day
+
+      // Set the current city name and data to local storage
+      localStorage.setItem("currentCityName", city);
+      localStorage.setItem("currentDayData", JSON.stringify(data.list[0]));
+    });
+}
+// Function to display the weather data for the city
+function displayWeatherData(data) {
+  // Get ids and set text content
+  document.getElementById("temp1").textContent = data[0].temp;
+  console.log(data[0].temp);
+}
+
+// Function to display the current day data
+function saveCity(city) {
+  let savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
+  if (!savedCities.includes(city)) {
+    savedCities.push(city);
+    localStorage.setItem("savedCities", JSON.stringify(savedCities));
+  }
+}
+
+function displaySavedCities() {
+  const savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
+}
